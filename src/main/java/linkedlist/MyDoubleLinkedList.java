f483e4aaef661b98420d6e834af633cc6f638fd9
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2020-01-17 21:15:09 -0800

    Created skeleton for MyDoubleLinkedList

---
+++ src/main/java/linkedlist/MyDoubleLinkedList.java
@@ -0,0 +1,129 @@
+package linkedlist;
+
+import java.util.Iterator;
+
+public class MyDoubleLinkedList<T> implements GLinkedList<T>, Iterator<T> {
+
+  // TODO - - - - - - - - - - - - - - - Member variables - - - - - - - - - - - - - - -
+
+
+  // TODO - - - - - - - - - - - - - - - Constructor(s) - - - - - - - - - - - - - - -
+
+
+
+  // TODO - - - - - - - - - - - - - - - Add - - - - - - - - - - - - - - -
+
+  @Override
+  public void addAtStart(T data) {
+
+  }
+
+  @Override
+  public void addAtEnd(T data) {
+
+  }
+
+  @Override
+  public void addAtIndex(T data, int index) {
+
+  }
+
+
+  // TODO - - - - - - - - - - - - - - - Set - - - - - - - - - - - - - - -
+
+  @Override
+  public void setAtStart(T data) {
+
+  }
+
+  @Override
+  public void setAtEnd(T data) {
+
+  }
+
+  @Override
+  public void setAtIndex(T data, int index) {
+
+  }
+
+
+  // TODO - - - - - - - - - - - - - - - Delete - - - - - - - - - - - - - - -
+
+  @Override
+  public void deleteAtStart() {
+
+  }
+
+  @Override
+  public void deleteAtEnd() {
+
+  }
+
+  @Override
+  public void deleteAtIndex(int index) {
+
+  }
+
+  @Override
+  public void deleteItem(T data) {
+
+  }
+
+
+  // TODO - - - - - - - - - - - - - - - Get - - - - - - - - - - - - - - -
+
+  @Override
+  public T getAtStart() {
+    return null;
+  }
+
+  @Override
+  public T getAtEnd() {
+    return null;
+  }
+
+  @Override
+  public T getAtIndex(int index) {
+    return null;
+  }
+
+
+  // TODO - - - - - - - - - - - - - - - Contains - - - - - - - - - - - - - - -
+
+  @Override
+  public boolean contains(T data) {
+    return false;
+  }
+
+
+  // TODO - - - - - - - - - - - - - - - Iterator callbacks - - - - - - - - - - - - - - -
+
+  @Override
+  public boolean hasNext() {
+    return false;
+  }
+
+  @Override
+  public T next() {
+    return null;
+  }
+
+
+  // TODO - - - - - - - - - - - - - - - Object callbacks - - - - - - - - - - - - - - -
+
+  @Override
+  public boolean equals(Object obj) {
+    return super.equals(obj);
+  }
+
+  @Override
+  public String toString() {
+    return super.toString();
+  }
+
+
+  // TODO - - - - - - - - - - - - - - - Node class - - - - - - - - - - - - - - -
+
+
+
+}
