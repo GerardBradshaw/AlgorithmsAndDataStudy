3d68eeab048cac9b29c17ee79b632ad865016aee
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2020-01-21 11:53:26 -0800

    Created seperate method to output LinkedList data

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,63 +9,6 @@ import java.util.*;
 public class HelloWorld {
 
   public static void main(String[] args) {
-
-    // Parameters
-    int numberOfDataPoints = 200;
-    int maxListSize = 5000000;
-
-    List<String> timeList = new ArrayList<>();
-    timeList.add("MapSize,AddToStartTime,GetAtEndTime,RemoveFromEndTime,FindRandomValueTime,RandomValueFound");
-    System.out.print("Running...");
-
-    for (int i = 2; i <= maxListSize; i = i + (maxListSize / numberOfDataPoints)) {
-      String timeListString = Integer.toString(i);
-
-      // Created using addAtEnd
-      long startTimeNs = System.nanoTime();
-      MyLinkedList list = createRandLinkedList(i);
-      long endTimeNs = System.nanoTime();
-      long timeElapsedNs = (endTimeNs - startTimeNs);
-
-      timeListString += "," + timeElapsedNs;
-
-
-      // getFromEnd
-      assert list != null;
-      startTimeNs = System.nanoTime();
-      list.getAtEnd();
-      endTimeNs = System.nanoTime();
-      timeElapsedNs = (endTimeNs - startTimeNs);
-
-      timeListString += "," + timeElapsedNs;
-
-
-      // removeFromEnd
-      startTimeNs = System.nanoTime();
-      list.deleteAtEnd();
-      endTimeNs = System.nanoTime();
-      timeElapsedNs = (endTimeNs - startTimeNs);
-
-      timeListString += "," + timeElapsedNs;
-
-
-      // contains
-      startTimeNs = System.nanoTime();
-      int randomInt = new Random().nextInt();
-      boolean found = list.contains(randomInt);
-      endTimeNs = System.nanoTime();
-      timeElapsedNs = (endTimeNs - startTimeNs);
-
-      timeListString += "," + timeElapsedNs + "," + found;
-
-      timeList.add(timeListString);
-    }
-
-    System.out.println("done!");
-
-    System.out.print("Writing to file...");
-    writeToFile("LinkedListEfficiency.txt", timeList);
-
   }
 
   private static MyLinkedList createRandLinkedList(int size) {
@@ -116,6 +59,64 @@ public class HelloWorld {
     } catch (UnsupportedEncodingException e) {
       System.out.println("failed. Unsupported encoding type.");
     }
+  }
+
+  private static void outputLinkedListEfficiency() {
+    // Parameters
+    int numberOfDataPoints = 200;
+    int maxListSize = 5000000;
+
+    List<String> timeList = new ArrayList<>();
+    timeList.add("MapSize,AddToStartTime,GetAtEndTime,RemoveFromEndTime,FindRandomValueTime,RandomValueFound");
+    System.out.print("Running...");
+
+    for (int i = 2; i <= maxListSize; i = i + (maxListSize / numberOfDataPoints)) {
+      String timeListString = Integer.toString(i);
+
+      // Created using addAtEnd
+      long startTimeNs = System.nanoTime();
+      MyLinkedList list = createRandLinkedList(i);
+      long endTimeNs = System.nanoTime();
+      long timeElapsedNs = (endTimeNs - startTimeNs);
+
+      timeListString += "," + timeElapsedNs;
+
+
+      // getFromEnd
+      assert list != null;
+      startTimeNs = System.nanoTime();
+      list.getLast();
+      endTimeNs = System.nanoTime();
+      timeElapsedNs = (endTimeNs - startTimeNs);
+
+      timeListString += "," + timeElapsedNs;
+
+
+      // removeFromEnd
+      startTimeNs = System.nanoTime();
+      list.removeLast();
+      endTimeNs = System.nanoTime();
+      timeElapsedNs = (endTimeNs - startTimeNs);
+
+      timeListString += "," + timeElapsedNs;
+
+
+      // contains
+      startTimeNs = System.nanoTime();
+      int randomInt = new Random().nextInt();
+      boolean found = list.contains(randomInt);
+      endTimeNs = System.nanoTime();
+      timeElapsedNs = (endTimeNs - startTimeNs);
+
+      timeListString += "," + timeElapsedNs + "," + found;
+
+      timeList.add(timeListString);
+    }
+
+    System.out.println("done!");
+
+    System.out.print("Writing to file...");
+    writeToFile("LinkedListEfficiency.txt", timeList);
   }
 
 }
