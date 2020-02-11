cc7ea00c2c1d388817a867e9d6daf9893978e784
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2020-02-11 11:25:14 -0800

    Updated GHashTable interface method names

--- src/main/java/hashtable/GHashTable.java
+++ src/main/java/hashtable/GHashTable.java
@@ -4,7 +4,7 @@ public interface GHashTable<K, V> {
 
   void insert(K key, V value);
   V get(K key);
-  void delete(K key);
-  void set(K key);
+  void remove(K key);
+  void set(K key, V value);
   boolean contains(K key);
 }
