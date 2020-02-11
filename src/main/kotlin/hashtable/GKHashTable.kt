1396cf1f558d98caae14d56793f8c2bed37998a0
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2020-02-11 11:32:58 -0800

    Added methods to GKHashTable interface

--- src/main/kotlin/hashtable/GKHashTable.kt
+++ src/main/kotlin/hashtable/GKHashTable.kt
@@ -1,4 +1,11 @@
 package hashtable
 
-interface GKHashTable {
+interface GKHashTable<K,V> {
+
+  fun insert(key: K, value: V)
+  fun get(key: K): V
+  fun remove(key: K)
+  fun set(key: K, value: V)
+  fun contains(key: K): Boolean
+
 }
