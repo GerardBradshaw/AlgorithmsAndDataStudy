be2712ef69cb150a0e7f54a4066750075b65c7fd
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2020-02-14 13:16:24 -0800

    Updated Kotlin interface for HashTable

--- src/main/kotlin/hashtable/GKHashTable.kt
+++ src/main/kotlin/hashtable/GKHashTable.kt
@@ -3,9 +3,10 @@ package hashtable
 interface GKHashTable<K,V> {
 
   fun insert(key: K, value: V)
-  fun get(key: K): V?
-  fun remove(key: K)
+  fun get(key: Any): V?
+  fun remove(key: Any)
   fun set(key: K, value: V)
-  fun contains(key: K): Boolean
+  fun contains(key: Any): Boolean
+  fun size(): Int
 
 }
