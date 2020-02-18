f821d711a83fcf8b3ad7f30360b7a3f39b3ecc72
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2020-02-18 15:43:04 -0800

    Completed writing tests for WordTable

--- src/test/kotlin/hashtable/WordTableTest.kt
+++ src/test/kotlin/hashtable/WordTableTest.kt
@@ -2,17 +2,17 @@ package hashtable
 
 import org.hamcrest.CoreMatchers
 import org.junit.Assert.assertThat
-import org.junit.Assert.fail
 import org.junit.Before
 import org.junit.Test
 
 class WordTableTest {
 
+  private lateinit var originalTable: WordTable
   private lateinit var table: WordTable
   private var emptyTable = WordTable()
 
   private val newWord = Word("new", "a new word!")
-  private val existingWord = Word("bad", "a song by Michael Jackson")
+  private val existingWord = Word("bad", "not good")
 
   @Before
   fun setup() {
@@ -22,116 +22,223 @@ class WordTableTest {
       Word("bad", "not good"),
       Word("eat", "to consume"),
       Word("nap", "a mini-sleep")))
-  }
+
+    originalTable = WordTable(arrayOf(
+      Word("pin", "a sharp thing"),
+      Word("use", "to use"),
+      Word("bad", "not good"),
+      Word("eat", "to consume"),
+      Word("nap", "a mini-sleep")))  }
 
   @Test
   fun insertWord_wordWithUniqueKey() {
-    fail()
+    table.insertWord(newWord)
+
+    val resultTable = WordTable(arrayOf(
+      newWord,
+      Word("pin", "a sharp thing"),
+      Word("use", "to use"),
+      Word("bad", "not good"),
+      Word("eat", "to consume"),
+      Word("nap", "a mini-sleep")))
+
+    assertThat(table, CoreMatchers.`is`(CoreMatchers.equalTo(resultTable)))
   }
 
   @Test
   fun insertWord_wordWithExistingKey() {
-    fail()
+    table.insertWord(existingWord)
+    assertThat(table, CoreMatchers.`is`(CoreMatchers.equalTo(originalTable)))
   }
 
   @Test
   fun insertWords_validArray() {
-    fail()
+    val arrayToInsert = arrayOf(
+      Word("car", "a personal motor vehicle"),
+      Word("war", "what is it good for?"),
+      null,
+      Word("oak", "a type of tree"))
+
+    table.insertWords(arrayToInsert)
+
+    val resultTable = WordTable(arrayOf(
+      Word("pin", "a sharp thing"),
+      Word("use", "to use"),
+      Word("bad", "not good"),
+      Word("car", "a personal motor vehicle"),
+      Word("war", "what is it good for?"),
+      Word("eat", "to consume"),
+      Word("nap", "a mini-sleep"),
+      Word("oak", "a type of tree")))
+
+    assertThat(table, CoreMatchers.`is`(CoreMatchers.equalTo(resultTable)))
   }
 
   @Test
   fun insertWords_partlyValidArray() {
-    fail()
+    val arrayToInsert = arrayOf(
+      Word("car", "a personal motor vehicle"),
+      existingWord,
+      newWord,
+      null,
+      Word("oak", "a type of tree"))
+
+    table.insertWords(arrayToInsert)
+
+    val resultTable = WordTable(arrayOf(
+      Word("pin", "a sharp thing"),
+      Word("use", "to use"),
+      Word("bad", "not good"),
+      Word("car", "a personal motor vehicle"),
+      newWord,
+      Word("eat", "to consume"),
+      Word("nap", "a mini-sleep"),
+      Word("oak", "a type of tree")))
+
+    assertThat(table, CoreMatchers.`is`(CoreMatchers.equalTo(resultTable)))
   }
 
   @Test
   fun insertWords_completelyInvalidArray() {
-    fail()
+    val arrayToInsert = arrayOf(
+      existingWord,
+      existingWord,
+      null,
+      null,
+      existingWord,
+      null)
+
+    table.insertWords(arrayToInsert)
+
+    assertThat(table, CoreMatchers.`is`(CoreMatchers.equalTo(originalTable)))
   }
 
   @Test
   fun insertWordsWithoutChangingTableSize_validArray() {
-    fail()
+    val arrayToInsert = arrayOf(
+      Word("car", "a personal motor vehicle"),
+      Word("war", "what is it good for?"),
+      null,
+      Word("oak", "a type of tree"))
+
+    table.insertWordsWithoutChangingTableSize(arrayToInsert)
+
+    val resultTable = WordTable(arrayOf(
+      Word("pin", "a sharp thing"),
+      Word("use", "to use"),
+      Word("bad", "not good"),
+      Word("car", "a personal motor vehicle"),
+      Word("war", "what is it good for?"),
+      Word("eat", "to consume"),
+      Word("nap", "a mini-sleep"),
+      Word("oak", "a type of tree")))
+
+    assertThat(table, CoreMatchers.`is`(CoreMatchers.equalTo(resultTable)))
   }
 
   @Test
   fun insertWordsWithoutChangingTableSize_partlyValidArray() {
-    fail()
+    val arrayToInsert = arrayOf(
+      Word("car", "a personal motor vehicle"),
+      existingWord,
+      newWord,
+      null,
+      Word("oak", "a type of tree"))
+
+    table.insertWordsWithoutChangingTableSize(arrayToInsert)
+
+    val resultTable = WordTable(arrayOf(
+      Word("pin", "a sharp thing"),
+      Word("use", "to use"),
+      Word("bad", "not good"),
+      Word("car", "a personal motor vehicle"),
+      newWord,
+      Word("eat", "to consume"),
+      Word("nap", "a mini-sleep"),
+      Word("oak", "a type of tree")))
+
+    assertThat(table, CoreMatchers.`is`(CoreMatchers.equalTo(resultTable)))
   }
 
   @Test
   fun insertWordsWithoutChangingTableSize_invalidArray() {
-    fail()
+    val arrayToInsert = arrayOf(
+      existingWord,
+      existingWord,
+      null,
+      null,
+      existingWord,
+      null)
+
+    table.insertWordsWithoutChangingTableSize(arrayToInsert)
+
+    assertThat(table, CoreMatchers.`is`(CoreMatchers.equalTo(originalTable)))
   }
 
   @Test
   fun get_validString() {
-    fail()
+    assertThat(table.get(existingWord.word), CoreMatchers.`is`(CoreMatchers.equalTo(existingWord)))
   }
 
   @Test
   fun get_invalidString() {
-    fail()
+    assertThat(table.get(newWord.word), CoreMatchers.`is`(CoreMatchers.nullValue()))
   }
 
-  @Test
+  @Test(expected = IllegalArgumentException::class)
   fun get_invalidKeyType() {
-    fail()
+    val n: Int = 10
+    table.get(n)
   }
 
   @Test
   fun remove_validString() {
-    fail()
+    table.remove(existingWord.word)
+
+    val resultTable = WordTable(arrayOf(
+      Word("pin", "a sharp thing"),
+      Word("use", "to use"),
+      Word("eat", "to consume"),
+      Word("nap", "a mini-sleep")))
+
+    assertThat(table, CoreMatchers.`is`(CoreMatchers.equalTo(resultTable)))
   }
 
   @Test
   fun remove_invalidString() {
-    fail()
+    table.remove(newWord.word)
+    assertThat(table, CoreMatchers.`is`(CoreMatchers.equalTo(originalTable)))
   }
 
-  @Test
+  @Test(expected = IllegalArgumentException::class)
   fun remove_invalidKeyType() {
-    fail()
+    val n: Int = 10
+    table.remove(n)
   }
 
   @Test
   fun contains_validString() {
-    fail()
+    assertThat(table.contains(existingWord.word), CoreMatchers.`is`(CoreMatchers.equalTo(true)))
   }
 
   @Test
   fun contains_invalidString() {
-    fail()
+    assertThat(table.contains(newWord.word), CoreMatchers.`is`(CoreMatchers.equalTo(false)))
   }
 
-  @Test
+  @Test(expected = IllegalArgumentException::class)
   fun contains_invalidKeyType() {
-    fail()
+    val n: Int = 10
+    table.contains(n)
   }
 
   @Test
   fun size_tableWithSomeWords() {
-    fail()
+    assertThat(table.size(), CoreMatchers.`is`(CoreMatchers.equalTo(5)))
   }
 
   @Test
   fun size_emptyTable() {
-    fail()
+    assertThat(emptyTable.size(), CoreMatchers.`is`(CoreMatchers.equalTo(0)))
   }
-
-  @Test
-  fun memorySize_tableWithSomeWords() {
-    fail()
-  }
-
-  @Test
-  fun memorySize_emptyTable() {
-    fail()
-  }
-
-  @Test
-  fun memorySize_resizedTableWithSomeWords() {
-    fail()
-  }
-
 }
