764cd530d208fbde4dca66b97c30c67982643118
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2020-02-19 11:54:10 -0800

    Added skeleton methods to Kotlin stack class

--- src/main/kotlin/stack/MyKStack.kt
+++ src/main/kotlin/stack/MyKStack.kt
@@ -1,4 +1,49 @@
 package stack
 
-class MyKStack {
+class MyKStack<T> {
+
+  private var top: Node<T>? = null
+  private var base: Node<T>? = null
+
+  /**
+   * Pushes input data to the top of the stack.
+   */
+  fun push(data: T) {
+    TODO()
+  }
+
+  /**
+   * Removes and returns the data from the top of the stack.
+   */
+  fun pop(): T {
+    TODO()
+  }
+
+  /**
+   * Returns the data from the top of the stack without removing it.
+   */
+  fun peek(): T {
+    TODO()
+  }
+
+  override fun equals(other: Any?): Boolean {
+    TODO()
+  }
+
+  override fun hashCode(): Int {
+    return toString().hashCode()
+  }
+
+  override fun toString(): String {
+    TODO()
+  }
+
+  private data class Node<T>(var data: T) {
+
+    var prev: Node<T>? = null
+
+    override fun toString(): String {
+      return data.toString()
+    }
+  }
 }
