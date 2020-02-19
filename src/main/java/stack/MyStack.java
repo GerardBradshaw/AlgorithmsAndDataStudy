e644a9a9ccde0768fb4b889cbf49c937151017d1
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2020-02-18 18:36:35 -0800

    Completed implementation of MyStack
    Not unit tested

--- src/main/java/stack/MyStack.java
+++ src/main/java/stack/MyStack.java
@@ -1,4 +1,69 @@
 package stack;
 
-public class MyStack {
+public class MyStack<T> {
+
+  private Node<T> top = null;
+  private Node<T> base = null;
+
+  public void push(T data) {
+    Node<T> newNode = new Node<>(data);
+    if (base == null) {
+      base = newNode;
+      top = newNode;
+
+    } else {
+      newNode.previous = top;
+      top = newNode;
+    }
+  }
+
+  public T peek() {
+    if (top == null)
+      return null;
+
+    return top.data;
+  }
+
+  public T pop() {
+    if (top == null)
+      return null;
+
+    Node<T> temp = top;
+    top = top.previous;
+    return temp.data;
+  }
+
+  @Override
+  public String toString() {
+    if (top == null)
+      return "empty";
+
+    StringBuilder builder = new StringBuilder();
+    builder.append("TOP ");
+
+    Node<T> currentNode = top;
+
+    while (currentNode != null) {
+      builder.append("<- [" + currentNode.toString() + "] ");
+      currentNode = currentNode.previous;
+    }
+
+    builder.append("<- BASE");
+    return builder.toString();
+  }
+
+  private static class Node<T> {
+    T data;
+    Node<T> previous;
+
+    Node(T data) {
+      this.data = data;
+    }
+
+    @Override
+    public String toString() {
+      return data.toString();
+    }
+  }
+
 }
