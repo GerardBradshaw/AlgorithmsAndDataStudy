167bb321d50e3f0a3d9fb766fe648cfd03389921
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2020-02-21 14:52:55 -0800

    Added Node data class to Kotlin Binary Tree

--- src/main/kotlin/tree/MyKBinaryTree.kt
+++ src/main/kotlin/tree/MyKBinaryTree.kt
@@ -1,4 +1,14 @@
 package tree
 
 class MyKBinaryTree {
+
+
+  private data class Node(var value: Int) {
+    var left: Node? = null
+    var right: Node? = null
+
+    override fun toString(): String {
+      return value.toString()
+    }
+  }
 }
