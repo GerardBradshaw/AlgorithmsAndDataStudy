b4f19c8f36a85b0f07e4c4cdb6681b53e4066a36
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2020-02-21 12:39:55 -0800

    Added empty methods to Kotlin heap

--- src/main/kotlin/heap/MyKHeap.kt
+++ src/main/kotlin/heap/MyKHeap.kt
@@ -1,4 +1,55 @@
 package heap
 
 class MyKHeap {
+
+  var data: Array<Int?>? = null
+
+  constructor(data: Array<Int?>) {
+    for (int in data) {
+      insert(int)
+    }
+  }
+
+  fun findMax(): Int {
+    TODO()
+  }
+
+  fun insert(int: Int?) {
+    TODO()
+  }
+
+  fun popMax(): Int {
+    TODO()
+  }
+
+  fun deleteMax() {
+    TODO()
+  }
+
+  fun size(): Int {
+    TODO()
+  }
+
+  fun isEmpty(): Boolean {
+    TODO()
+  }
+
+  override fun equals(other: Any?): Boolean {
+    TODO()
+  }
+
+  override fun toString(): String {
+    TODO()
+  }
+
+  override fun hashCode(): Int {
+    TODO()
+  }
+
+
+
+
+
+
+
 }
