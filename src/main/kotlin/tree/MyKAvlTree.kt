ebb674f38f2e5c0bbc3540fa7e67e77bbaff0c42
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2020-02-24 15:51:30 -0800

    Completed insert methods for AVL tree

--- src/main/kotlin/tree/MyKAvlTree.kt
+++ src/main/kotlin/tree/MyKAvlTree.kt
@@ -13,6 +13,7 @@ class MyKAvlTree {
   // ---------------- Public methods ----------------
 
   fun insert(value: Int) {
+    println("Inserting $value")
     var current = head
 
     if (current == null) {
@@ -21,29 +22,37 @@ class MyKAvlTree {
       return
     }
 
+    val newNode = Node(value)
     while (current != null) {
-      if (value > current.value) {
+      if (value >= current.value) {
 
-        val right = current.right
-        if (right == null) {
-          current.right = Node(value)
+        if (current.right == null) {
+          current.right = newNode
+          newNode.parent = current
+          size++
+
+          balanceTree(newNode)
           break
         }
-        else current = right
+
+        else current = current.right
+
 
       }
-      else if (value <= current.value) {
+      else if (value < current.value) {
 
-        val left = current.left
-        if (left == null) {
-          current.left = Node(value)
+        if (current.left == null) {
+          current.left = newNode
+          newNode.parent = current
+          size++
+
+          balanceTree(newNode)
           break
         }
-        else current = left
+
+        else current = current.left
       }
     }
-    size++
-    balanceTree()
   }
 
   fun delete(value: Int) {
@@ -72,7 +81,6 @@ class MyKAvlTree {
         else current = right
       }
     }
-    balanceTree()
   }
 
   fun contains(value: Int): Boolean {
@@ -103,28 +111,198 @@ class MyKAvlTree {
     return size
   }
 
+  fun getHead(): Node? {
+    return head
+  }
+
 
   // ---------------- Helpers ----------------
 
-  private fun balanceTree() {
-    TODO()
+  private fun balanceTree(insertedNode: Node) {
+    val c = insertedNode
+    val b: Node? = c.parent
+    val a: Node? = b?.parent
+
+    var current: Node? = a
+    var currentChild = b
+
+    while (current != null) {
+      val balanceFactor = current.getBalanceFactor()
+
+      // Left-heavy if balanceFactor == 2
+      if (balanceFactor == 2) {
+        val childBalFactor = currentChild?.getBalanceFactor()
+
+        if (childBalFactor == 1) {
+          println("LL rotation to insert ${insertedNode.value}")
+          rotationLL(current)
+          return
+        } else if (childBalFactor == -1) {
+          println("LR rotation to insert ${insertedNode.value}")
+          rotationLR(current)
+          return
+        } else return
+      }
+
+      // Right-heavy if balanceFactor == -2
+      else if (balanceFactor == -2) {
+        val childBalFactor = currentChild?.getBalanceFactor()
+
+        if (childBalFactor == 1) {
+          println("RL rotation to insert ${insertedNode.value}")
+          rotationRL(current)
+          return
+        } else if (childBalFactor == -1) {
+          println("RR rotation to insert ${insertedNode.value}")
+          rotationRR(current)
+          return
+        } else return
+      }
+      currentChild = current
+      current = current.parent
+    }
+
+    /*
+while (b != null && a != null) {
+  val aBal = a.getBalanceFactor()
+  val bBal = b.getBalanceFactor()
+
+  if (aBal + bBal == -2) {
+    doRRRotation(a)
+    println("RR rotation")
+    return
+  }
+  else if (aBal + bBal == 2) {
+    doLLRotation(a)
+    println("LL rotation")
+    return
+  }
+  else if (aBal - bBal == 2) {
+    doLRRotation(a)
+    println("LR rotation")
+    return
+  }
+  else if (-aBal + bBal == 2) {
+    doRLRotation(a)
+    println("RL rotation")
+    return
+  }
+  else if (aBal + bBal == 0)
+    return
+
+  b = b.parent
+  a = b?.parent
+}
+ */
+  }
+
+  private fun rotationLL(a: Node) {
+    val parent = a.parent
+    val b = a.left
+    val c = b?.left
+    val bR = b?.right
+
+    if (parent != null) {
+      if (parent.left == a)
+        parent.left = b
+      else if (parent.right == a)
+        parent.right = b
+      else return
+    }
+    else head = b
+
+    b?.parent = parent
+    a.parent = b
+    b?.left = c
+    b?.right = a
+    a.left = bR
+    bR?.parent = a
+  }
+
+  private fun rotationRR(a: Node) {
+    val parent = a.parent
+    val b = a.right
+    val c = b?.right
+    val bL = b?.left
+
+    if (parent != null) {
+      if (parent.left == a)
+        parent.left = b
+      else if (parent.right == a)
+        parent.right = b
+    }
+    else head = b
+
+    b?.parent = parent
+    a.parent = b
+    a.right = bL
+    bL?.parent = a
+    b?.left = a
+    b?.right = c
+  }
+
+  private fun rotationLR(a: Node) {
+    val parent = a.parent
+    val b = a.left
+    val c = b?.right
+    val cL = c?.left
+    val cR = c?.right
+
+    if (parent != null)
+      if (parent.left == a)
+        parent.left = c
+      else if (parent.right == a)
+        parent.right = c
+    else head = c
+
+    c?.parent = parent
+    a.parent = c
+    b?.parent = c
+    c?.left = b
+    c?.right = a
+    b?.right = cL
+    a.left = cR
+    cL?.parent = b
+    cR?.parent = a
+  }
+
+  private fun rotationRL(a: Node) {
+    val parent = a.parent
+    val b = a.right
+    val c = b?.left
+    val cL = c?.left
+    val cR = c?.right
+
+    if (parent != null) {
+      if (parent.left == a)
+        parent.left = c
+      else if (parent.right == a)
+        parent.right = c
+    }
+    else head = c
+
+    c?.parent = parent
+    a.parent = c
+    b?.parent = c
+    c?.left = a
+    c?.right = b
+    a.right = cL
+    b?.left = cR
+    cL?.parent = a
+    cR?.parent = b
   }
 
   private fun deleteHead() {
-
-    size--
     TODO()
   }
 
   private fun deleteLeftNodeOf(node: Node) {
-    
-    size--
+    println(node.value)
     TODO()
   }
 
   private fun deleteRightNodeOf(node: Node) {
-
-    size--
+    println(node.value)
     TODO()
   }
 
@@ -144,12 +322,32 @@ class MyKAvlTree {
 
   // ---------------- Node class ----------------
 
-  private data class Node(var value: Int) {
+  data class Node(var value: Int, var parent: Node? = null) {
     var left: Node? = null
     var right: Node? = null
 
     override fun toString(): String {
       return value.toString()
+    }
+
+    fun getLeftHeight(): Int {
+      val tempLeft = left
+      if (tempLeft != null)
+        return maxOf(tempLeft.getLeftHeight(), tempLeft.getRightHeight()) + 1
+
+      return 0
+    }
+
+    fun getRightHeight(): Int {
+      val tempRight = right
+      if (tempRight != null)
+        return maxOf(tempRight.getLeftHeight(), tempRight.getRightHeight()) + 1
+
+      return 0
+    }
+
+    fun getBalanceFactor(): Int {
+      return getLeftHeight() - getRightHeight()
     }
   }
 }
