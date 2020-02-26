7ba141792b82d8394f8219d992f772d96ccdd573
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2020-02-26 13:56:50 -0800

    Completed delete()

--- src/main/kotlin/tree/MyKAvlTree.kt
+++ src/main/kotlin/tree/MyKAvlTree.kt
@@ -1,6 +1,5 @@
 package tree
 
-import java.io.InvalidObjectException
 import java.lang.NullPointerException
 
 class MyKAvlTree {
@@ -63,8 +62,7 @@ class MyKAvlTree {
       throw NullPointerException()
 
     while (current != null) {
-      if (current.value == value)
-        deleteNode(current)
+      if (current.value == value) deleteNode(current)
 
       current = if (value > current.value)
         current.right
@@ -108,46 +106,78 @@ class MyKAvlTree {
 
   // ---------------- Helpers ----------------
 
-  private fun balanceTree(insertedNode: Node, fullBalance: Boolean = false) {
-    val c = insertedNode
-    val b: Node? = c.parent
-    val a: Node? = b?.parent
+  private fun balanceTree(node: Node?, fullBalance: Boolean = false) {
+    /*
+    Scenarios:
+    1. Node has parent and grandparent
+      current
+         |
+    currentChild
+         |
+        node
 
-    var current: Node? = a
-    var currentChild = b
+    2. Node has parent but no grandparent
+
+     */
+
+    if (node == null) return
+
+    var currentChild: Node? = node.parent
+    var current: Node? = currentChild?.parent
+
+    if (currentChild != null && current == null) {
+      if (node == currentChild.left) current = currentChild.right
+      else current = currentChild.left
+    }
 
     while (current != null) {
       val balanceFactor = current.getBalanceFactor()
 
       if (balanceFactor >= 2) {
-        val childBalanceFactor = currentChild?.getBalanceFactor()
-
-        if (childBalanceFactor == 1) {
-          rotationLL(current)
-          if (!fullBalance) return
-        }
-        else {
-          rotationLR(current)
-          if (!fullBalance) return
-        }
+        leftRotation(currentChild)
+        if (fullBalance) balanceTree(currentChild)
+        else return
       }
-
       else if (balanceFactor <= -2) {
-        val childBalFactor = currentChild?.getBalanceFactor()
-
-        if (childBalFactor == 1) {
-          rotationRL(current)
-          if (!fullBalance) return
-        }
-        else {
-          rotationRR(current)
-          if (!fullBalance) return
-        }
+        rightRotation(currentChild)
+        if (fullBalance) balanceTree(currentChild)
+        else return
       }
       else {
         currentChild = current
         current = current.parent
       }
+    }
+  }
+
+  private fun leftRotation(pivotChild: Node?) {
+    /*
+    Scenarios
+
+    LL rotation (pivotChild balance factor = 1)
+                pivot
+                /   \
+      pivotChild    ...
+              /
+            ...
+
+              OR
+
+    LR rotation (pivotChild balance factor = -1)
+                 pivot
+                /     \
+      pivotChild      ...
+               \
+               ...
+     */
+
+    val pivot = pivotChild?.parent
+
+    if (pivot != null) {
+      val pivotChildBalanceFactor = pivotChild.getBalanceFactor()
+
+      if (pivotChildBalanceFactor == 1) rotationLL(pivot)
+      else rotationLR(pivot)
     }
   }
 
@@ -174,6 +204,62 @@ class MyKAvlTree {
     bR?.parent = a
   }
 
+  private fun rotationLR(a: Node) {
+    val parent = a.parent
+    val b = a.left
+    val c = b?.right
+    val cL = c?.left
+    val cR = c?.right
+
+    if (parent != null)
+      if (parent.left == a)
+        parent.left = c
+      else if (parent.right == a)
+        parent.right = c
+      else head = c
+
+    c?.parent = parent
+    a.parent = c
+    b?.parent = c
+    c?.left = b
+    c?.right = a
+    b?.right = cL
+    a.left = cR
+    cL?.parent = b
+    cR?.parent = a
+  }
+
+  private fun rightRotation(pivotChild: Node?) {
+    /*
+    Scenarios
+
+    RR rotation (pivotChild balance factor = -1)
+                pivot
+                /   \
+              ...   pivotChild
+                     \
+                    ...
+
+              OR
+
+    RL rotation (pivotChild balance factor = 1)
+                 pivot
+                /     \
+              ...     pivotChild
+                     /
+                   ...
+ */
+
+    val pivot = pivotChild?.parent
+
+    if (pivot != null) {
+      val childBalFactor = pivotChild.getBalanceFactor()
+
+      if (childBalFactor == 1) rotationRL(pivot)
+      else rotationRR(pivot)
+    }
+  }
+
   private fun rotationRR(a: Node) {
     val parent = a.parent
     val b = a.right
@@ -194,31 +280,6 @@ class MyKAvlTree {
     bL?.parent = a
     b?.left = a
     b?.right = c
-  }
-
-  private fun rotationLR(a: Node) {
-    val parent = a.parent
-    val b = a.left
-    val c = b?.right
-    val cL = c?.left
-    val cR = c?.right
-
-    if (parent != null)
-      if (parent.left == a)
-        parent.left = c
-      else if (parent.right == a)
-        parent.right = c
-    else head = c
-
-    c?.parent = parent
-    a.parent = c
-    b?.parent = c
-    c?.left = b
-    c?.right = a
-    b?.right = cL
-    a.left = cR
-    cL?.parent = b
-    cR?.parent = a
   }
 
   private fun rotationRL(a: Node) {
@@ -247,49 +308,7 @@ class MyKAvlTree {
     cR?.parent = b
   }
 
-  private fun deleteLeaf(node: Node) {
-    val parent = node.parent
-
-    if (parent != null) {
-      if (parent.left == node)
-        parent.left = null
-      else if (parent.right == node)
-        parent.right = null
-    }
-    else head = null
-  }
-
-  private fun deleteNodeWithRightChildOnly(node: Node, right: Node) {
-    if (node.right != right)
-      throw InvalidObjectException("Tried to delete node with only right child, but provided child node was not to the right of provided parent node.")
-
-    val parent = node.parent
-
-    if (parent != null) {
-      if (parent.left == node)
-        parent.left = right
-      else if (parent.right == node)
-        parent.right = right
-    }
-    else head = right
-  }
-
-  private fun deleteNodeWithLeftChildOnly(node: Node, left: Node) {
-    if (node.left != left)
-      throw InvalidObjectException("Tried to delete node with only left child, but provided child node was not to the left of provided parent node.")
-
-    val parent = node.parent
-
-    if (parent != null) {
-      if (parent.left == node)
-        parent.left = left
-      else if (parent.right == node)
-        parent.right = left
-    }
-    else head = left
-  }
-
-  private fun connectParentAndChild(parent: Node?, child: Node) {
+  private fun linkParentToChild(parent: Node?, child: Node) {
     if (parent == null) {
       head = child
       child.parent = null
@@ -301,6 +320,8 @@ class MyKAvlTree {
       else
         parent.right = child
     }
+
+    balanceTree(child, true)
   }
 
   private fun removeReferencesFromNode(xNode: Node) {
@@ -309,67 +330,110 @@ class MyKAvlTree {
     xNode.right = null
   }
 
-  private fun deleteNodeWithTwoChildren(xNode: Node) {
+  private fun deleteLeaf(xNode: Node) {
     val parent = xNode.parent
-    val inheritor = xNode.left
-    val right = xNode.right
+    val parentLeft = parent?.left
+    val parentRight = parent?.right
 
-    if (inheritor == null) {
-      if (right != null)
-        connectParentAndChild(parent, right)
-    }
+    if (parent == null) head = null
+
     else {
-      connectParentAndChild(parent, inheritor)
-      inheritor.right = right
-      right?.parent = inheritor
+      if (parentLeft == xNode) {
+        parent.left = null
+        balanceFromSubTree(parentRight)
+        balanceFromHead()
+      }
 
-      if (right == null)
-        balanceTree(inheritor, true)
-
-      else
-        insertSubTree(right, inheritor)
+      else {
+        parent.right = null
+        balanceFromSubTree(parentLeft)
+        balanceFromHead()
+      }
     }
     removeReferencesFromNode(xNode)
   }
 
-  private fun insertSubTree(subTree: Node, start: Node? = null) {
-    var current: Node? = start
-    val value = subTree.value
+  private fun balanceFromHead() {
+    val left = head?.left
+    val right = head?.right
 
-    while (current != null) {
-      current = if (value > current.value) {
-        if (current.left != null)
-          current.left
-        else break
-      } else {
-        if (current.right != null)
-          current.right
-        else break
+    if (left != null) {
+      val leftLeft = left.left
+      val leftRight = left.right
+      when {
+        leftLeft != null -> balanceTree(leftLeft)
+        leftRight != null -> balanceTree(leftRight)
       }
     }
-
-    connectParentAndChild(current, subTree)
-    balanceTree(subTree, true)
+    if (right != null) {
+      val rightLeft = right.left
+      val rightRight = right.right
+      when {
+        rightLeft != null -> balanceTree(rightLeft)
+        rightRight != null -> balanceTree(rightRight)
+      }
+    }
   }
 
-  private fun deleteNode(node: Node) {
-    val left = node.left
-    val right = node.right
+  private fun balanceFromSubTree(subTree: Node?) {
+    if (subTree != null) {
+      val left = subTree.left
+      val right = subTree.right
 
-    if (left == null)
-      if (right == null)
-        deleteLeaf(node)
-      else
-        deleteNodeWithRightChildOnly(node, right)
-
-    else if (right == null)
-      deleteNodeWithLeftChildOnly(node, left)
-
-    else {
-      deleteNodeWithTwoChildren(node)
+      when {
+        left != null -> balanceTree(left, true)
+        right != null -> balanceTree(right, true)
+        else -> balanceTree(subTree, true)
+      }
     }
+  }
 
-    node.parent = null
+  private fun deleteNode(xNode: Node) {
+    /* Naming scheme (xNode = node to delete)
+           parent
+             |
+           xNode
+         /      \
+        cL      cR
+       /  \    /  \
+     cLL cLR cRL  cRR
+     */
+
+    val parent = xNode.parent
+    val childLeft = xNode.left
+    val childRight = xNode.right
+
+    val childLeftRight = childLeft?.right
+
+    if (childLeft == null) {
+      if (childRight == null) {
+        deleteLeaf(xNode)
+      }
+      else {
+        linkParentToChild(parent, childRight)
+      }
+    }
+    else {
+      linkParentToChild(parent, childLeft)
+      if (childRight != null) {
+        linkParentToChild(childLeft, childRight)
+        balanceFromSubTree(childRight)
+      }
+      if (childLeftRight != null) insertSubTree(childLeftRight)
+    }
+    removeReferencesFromNode(xNode)
+    println("Deleted ${xNode.value}")
+  }
+
+  private fun insertSubTree(subTree: Node?) {
+    if (subTree == null) return
+    insert(subTree.value)
+
+    val left = subTree.left
+    val right = subTree.right
+
+    if (left != null) insertSubTree(left)
+    if (right != null) insertSubTree(right)
   }
 
   // ---------------- Any callbacks ----------------
