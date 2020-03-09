0a2b0ed35fe06e9e377905316cf54fb32c9aee76
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2020-03-09 13:23:03 -0700

    Completed Trie class in Kotlin
    Not unit tested

--- src/main/kotlin/trie/MyKTrie.kt
+++ src/main/kotlin/trie/MyKTrie.kt
@@ -1,4 +1,176 @@
 package trie
 
+import array.MyKStringBuilder
+import java.lang.Exception
+
 class MyKTrie {
+
+  // ---------------- Properties ----------------
+
+  private val head = Node()
+  var wordCount = 0
+
+
+  // ---------------- Public methods ----------------
+
+  fun insert(word: String) {
+    var current = head
+
+    for (char in word) {
+      if (!current.children.contains(char)) {
+        current.children[char] = Node(char)
+      }
+      current = current.children[char] ?: throw Exception()
+    }
+    current.isCompleteWord = true
+    wordCount++
+  }
+
+  fun delete(word: String) {
+    var current = head
+    var keepIndex = 0
+    var keepNode: Node? = null
+
+    for (index in 0..word.length - 2) {
+    //for (index in word.indices) {
+      current = current.children[word[index]] ?: return
+      if (current.isCompleteWord || current.children.size > 1) {
+        keepNode = current
+        keepIndex = index
+      }
+    }
+    current = current.children[word[word.length - 1]] ?: return
+    current.isCompleteWord = false
+    wordCount--
+    if (current.children.size > 0) return
+
+    if (keepNode == null) safeDelete(word, 0, head)
+    else safeDelete(word, keepIndex, keepNode)
+  }
+
+  fun contains(word: String): Boolean {
+    var current = head
+
+    for (char in word) {
+      current = current.children[char] ?: return false
+    }
+    return current.isCompleteWord
+  }
+
+  fun isEmpty(): Boolean {
+    return head.children.isEmpty()
+  }
+
+  fun printWordList() {
+    if (head.children.isEmpty()) println("(empty)")
+    else printHelper(head, MyKStringBuilder())
+  }
+
+  // ---------------- Helpers ----------------
+
+  private fun safeDelete(word: String, lastIndexToKeep: Int, lastNodeToKeep: Node) {
+    val indexLast = word.length - 1
+
+    if (indexLast <= lastIndexToKeep) return
+
+    var indexDelete = lastIndexToKeep + 1
+    var charDelete = word[indexDelete]
+    var nodeDelete = lastNodeToKeep.children[charDelete]
+
+    var indexNext = indexDelete + 1
+
+    if (indexNext > indexLast) {
+      lastNodeToKeep.children.remove(charDelete)
+
+      return
+    }
+
+    var charNext = word[indexNext]
+    var nodeNext = nodeDelete?.children?.get(charNext)
+
+    while (nodeNext != null && !nodeNext.isCompleteWord) {
+      nodeDelete?.children?.remove(charDelete)
+      indexDelete++
+      charDelete = word[indexDelete]
+      nodeDelete = nodeNext
+      indexNext++
+      charNext = word[indexNext]
+      nodeNext = nodeDelete.children.get(charNext)
+    }
+    if (nodeNext != null) nodeNext.isCompleteWord = false
+  }
+
+  private fun printHelper(node: Node, builder: MyKStringBuilder) {
+    val children = node.children
+    val char = node.char
+
+    builder.append(char)
+
+    if (node.isCompleteWord) println(builder.toString())
+
+    if (children.isNotEmpty()) {
+      for (child in children.values) {
+        printHelper(child, MyKStringBuilder(builder.toString()))
+      }
+    }
+  }
+
+  private fun toStringHelper(node: Node, builder: MyKStringBuilder, wordList: MyKStringBuilder): MyKStringBuilder {
+    val children = node.children
+    val char = node.char
+
+    builder.append(char)
+
+    if (node.isCompleteWord) return wordList.append(builder.toString())
+
+    if (children.isNotEmpty()) {
+      for (child in children.values) {
+        printHelper(child, MyKStringBuilder(builder.toString()))
+      }
+    }
+    return wordList
+  }
+
+
+  // ---------------- Any methods ----------------
+
+  override fun equals(other: Any?): Boolean {
+    if (other !is MyKTrie) return false
+    return other.toString() == toString()
+  }
+
+  override fun hashCode(): Int {
+    return toString().hashCode()
+  }
+
+  override fun toString(): String {
+    return toStringHelper(head, MyKStringBuilder(), MyKStringBuilder()).toString()
+  }
+
+  // ---------------- Node class ----------------
+
+  data class Node(val char: Char? = null) {
+    val children = HashMap<Char, Node>()
+    var isCompleteWord = false
+
+    override fun equals(other: Any?): Boolean {
+      if (other !is Node) return false
+      if (other.char == char) return true
+      return false
+    }
+
+    override fun toString(): String {
+      val builder = MyKStringBuilder().append(char ?: '*').append(" -> [").append(if (isCompleteWord) "*, " else "-, ")
+
+      for (key in children.keys) {
+        builder.append(key).append(", ")
+      }
+      builder.removeEnd(2)
+      return builder.append("]").toString()
+    }
+
+    override fun hashCode(): Int {
+      return toString().hashCode()
+    }
+  }
 }
