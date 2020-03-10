04bfaf326c09943b14373c7efab8c2d43d7a816e
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2020-03-10 14:11:39 -0700

    Added empty methods to AdjListGraph
    To be completed once I've created a Hash Set

--- src/main/kotlin/graph/MyAdjListGraph.kt
+++ src/main/kotlin/graph/MyAdjListGraph.kt
@@ -1,4 +1,45 @@
 package graph
 
-class MyAdjListGraph {
+//import set.MyHashSet
+
+class MyAdjListGraph<T : Comparable<T>> {
+
+  // ---------------- Member variables ----------------
+  //private val vertices: MyHashSet<Node<T>> = MyHashSet()
+
+  var numberOfNodes = 0
+    private set
+
+  var numberOfEdges = 0
+    private set
+
+
+  // ---------------- Public methods ----------------
+
+  fun insert(vertex: T, edges: Collection<T>) {
+    TODO()
+  }
+
+  fun addVertex(vertex: T) {
+    //vertices.add(Node(vertex))
+  }
+
+  fun addEdge(source: T, destination: T) {
+    //for (node in vertices) {
+    // if (node.vertex == source) {
+    //    node.edges.add(Node(destination))
+    //    return
+    //  }
+    //}
+
+    TODO()
+  }
+
+
+  // ---------------- Data class ----------------
+  data class Node<T : Comparable<T>>(var vertex: T) {
+
+    //val edges: MyHashSet<Node<T>> = MyHashSet()
+
+  }
 }
