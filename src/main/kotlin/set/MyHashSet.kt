5142050ad0b8f14162a4802c817429b9804b6cce
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2020-03-10 14:20:22 -0700

    Added empty methods to MyHashTable
    To be completed after a HashMap has been created

--- src/main/kotlin/set/MyHashSet.kt
+++ src/main/kotlin/set/MyHashSet.kt
@@ -1,4 +1,47 @@
 package set
 
-class MyHashSet {
+class MyHashSet<T> : Set<T>, Iterable<T> {
+
+  // Member variables
+
+  
+
+  // Public methods
+
+  fun insert(element: T) {
+    TODO()
+  }
+
+  fun delete(element: T) {
+    TODO()
+  }
+
+  override val size: Int
+    get() = TODO()
+
+  override fun contains(element: T): Boolean {
+    TODO()
+  }
+
+  override fun containsAll(elements: Collection<T>): Boolean {
+    TODO()
+  }
+
+  override fun isEmpty(): Boolean {
+    TODO()
+  }
+
+  override fun iterator(): Iterator<T> {
+    TODO()
+  }
+
+
+  // Private methods
+
+  private fun getHash(element: T): Int {
+    //return element.hashCode() %
+    TODO()
+  }
+
+
 }
