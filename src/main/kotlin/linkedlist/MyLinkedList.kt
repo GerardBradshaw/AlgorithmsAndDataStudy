0d08b6557ea84a5b054a6d7b2bd39eeaf22175fd
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2020-03-13 17:34:12 -0700

    Rewrote equals() and hashCode() for MyLinkedList
    Refactored comments and field names

--- src/main/kotlin/linkedlist/MyLinkedList.kt
+++ src/main/kotlin/linkedlist/MyLinkedList.kt
@@ -5,23 +5,23 @@ import java.lang.NullPointerException
 
 class MyLinkedList() : GLinkedList<Int>, Iterable<Int> {
 
+  // ---------------- Fields & Constructor ----------------
+
+  private var head: Node? = null
+
   constructor(vararg ints: Int) : this() {
     for (index in (ints.size - 1) downTo 0) {
       addFirst(ints[index])
     }
   }
 
-  // - - - - - - - - - - - - - - - Properties/Fields - - - - - - - - - - - - - - -
 
-  private var headNode: Node? = null
-
-
-  // - - - - - - - - - - - - - - - Add - - - - - - - - - - - - - - -
+  // ---------------- Public fun ----------------
 
   override fun addFirst(data: Int) {
     val newNode = Node(data)
 
-    headNode?.let { newNode.linkedNode = it }
+    head?.let { newNode.next = it }
     /*
     Better than:
       if (headNode != null) newNode.linkedNode = headNode
@@ -32,7 +32,7 @@ class MyLinkedList() : GLinkedList<Int>, Iterable<Int> {
       In the first method, headNode could become null between the check and assignment of newNode.linkedNode.
      */
 
-    headNode = newNode
+    head = newNode
   }
 
   override fun addLast(data: Int) {
@@ -40,18 +40,18 @@ class MyLinkedList() : GLinkedList<Int>, Iterable<Int> {
 
     // If the list is null, make newNode the headNode immediately.
     // Otherwise, iterate to the end of the list and add it there.
-    if (headNode == null) {
-      headNode = newNode
+    if (head == null) {
+      head = newNode
 
     } else {
-      var currentNode = headNode
+      var currentNode = head
 
       // Iterate to the end of the list, then link newNode to the previous last node
-      while (currentNode?.linkedNode != null) {
-        currentNode = currentNode.linkedNode
+      while (currentNode?.next != null) {
+        currentNode = currentNode.next
       }
 
-      currentNode?.linkedNode = newNode
+      currentNode?.next = newNode
     }
   }
 
@@ -60,7 +60,7 @@ class MyLinkedList() : GLinkedList<Int>, Iterable<Int> {
 
     // Set up for iteration
     val newNode = Node(data)
-    var currentNode = headNode
+    var currentNode = head
     var currentIndex = 0
 
     // If index is 0, add newNode to start.
@@ -73,51 +73,48 @@ class MyLinkedList() : GLinkedList<Int>, Iterable<Int> {
       // Loop through this to find index. If found, add and return. Otherwise, add to last index or throw exception.
       while (currentNode != null && currentIndex < index) {
         if (currentIndex == index - 1) {
-          newNode.linkedNode = currentNode.linkedNode
-          currentNode.linkedNode = newNode
+          newNode.next = currentNode.next
+          currentNode.next = newNode
           return
         }
-        currentNode = currentNode.linkedNode
+        currentNode = currentNode.next
         currentIndex += 1
       }
     }
 
     // If the index is the last position, add it there. Otherwise, throw an exception.
-    if (currentIndex == index) currentNode?.linkedNode = newNode else throw IndexOutOfBoundsException()
+    if (currentIndex == index) currentNode?.next = newNode else throw IndexOutOfBoundsException()
   }
 
-
-  // - - - - - - - - - - - - - - - Set - - - - - - - - - - - - - - -
-
   override fun setFirst(data: Int) {
-    if (headNode == null) {
+    if (head == null) {
       throw NullPointerException()
 
     } else {
       val newNode = Node(data)
-      newNode.linkedNode = headNode?.linkedNode
-      headNode = newNode
+      newNode.next = head?.next
+      head = newNode
     }
   }
 
   override fun setLast(data: Int) {
     // If the list is null, make newNode the headNode immediately.
     // Otherwise, iterate to the end of the list and add it there.
-    if (headNode == null) {
+    if (head == null) {
       throw NullPointerException()
 
     } else {
-      var currentNode = headNode
+      var currentNode = head
 
       // Iterate to the end and link the last Node to newNode
-      while (currentNode?.linkedNode != null) currentNode = currentNode.linkedNode
+      while (currentNode?.next != null) currentNode = currentNode.next
       currentNode?.data = data
     }
   }
 
   override fun setAtIndex(data: Int, index: Int) {
     if (index < 0) throw IndexOutOfBoundsException()
-    if (headNode == null) throw NullPointerException()
+    if (head == null) throw NullPointerException()
 
     // If index is 0, set headNode to newNode.
     // Otherwise, check if index is in range and add it there.
@@ -126,7 +123,7 @@ class MyLinkedList() : GLinkedList<Int>, Iterable<Int> {
       return
 
     } else {
-      var currentNode = headNode
+      var currentNode = head
       var currentIndex = 0
 
       while (currentNode != null && currentIndex <= index) {
@@ -135,7 +132,7 @@ class MyLinkedList() : GLinkedList<Int>, Iterable<Int> {
           return
         }
 
-        currentNode = currentNode.linkedNode
+        currentNode = currentNode.next
         currentIndex += 1
       }
     }
@@ -144,165 +141,161 @@ class MyLinkedList() : GLinkedList<Int>, Iterable<Int> {
     throw IndexOutOfBoundsException()
   }
 
-
-  // - - - - - - - - - - - - - - - Delete - - - - - - - - - - - - - - -
-
   override fun removeFirst() {
-    if (headNode == null) throw NullPointerException()
+    if (head == null) throw NullPointerException()
 
     // If the list only contains headNode, delete it
     // Otherwise, make headNode's linkedNode null and make the second Node the new head
-    if (headNode?.linkedNode == null) {
-      headNode = null
+    if (head?.next == null) {
+      head = null
 
     } else {
-      val newHeadNode = headNode?.linkedNode
-      headNode?.linkedNode = null
-      headNode = newHeadNode
+      val newHeadNode = head?.next
+      head?.next = null
+      head = newHeadNode
     }
   }
 
   override fun removeLast() {
-    if (headNode == null) throw NullPointerException()
+    if (head == null) throw NullPointerException()
 
     // If the list only contains headNode, delete it
     // Otherwise, iterate to the end of the list and make the second-to-last Node link null (unlink the last Node)
-    if (headNode?.linkedNode == null) {
-      headNode = null
+    if (head?.next == null) {
+      head = null
 
     } else {
-      var currentNode = headNode
+      var currentNode = head
 
       // Iterate until currentNode is the second-to-last Node, then delete it's reference to the last Node
-      while (currentNode?.linkedNode?.linkedNode != null) currentNode = currentNode.linkedNode
-      currentNode?.linkedNode = null
+      while (currentNode?.next?.next != null) currentNode = currentNode.next
+      currentNode?.next = null
     }
   }
 
   override fun removeAtIndex(index: Int) {
-    if (headNode == null) throw NullPointerException()
+    if (head == null) throw NullPointerException()
 
     if (index == 0) {
       removeFirst()
       return
     }
 
-    var currentNode = headNode
+    var currentNode = head
     var currentIndex = 0
 
-    while (currentNode?.linkedNode != null && currentIndex < index) {
+    while (currentNode?.next != null && currentIndex < index) {
       if (currentIndex == index - 1) {
-        val newRef = currentNode.linkedNode?.linkedNode
-        currentNode.linkedNode?.linkedNode = null
-        currentNode.linkedNode = newRef
+        val newRef = currentNode.next?.next
+        currentNode.next?.next = null
+        currentNode.next = newRef
         return
       }
 
-      currentNode = currentNode.linkedNode
+      currentNode = currentNode.next
       currentIndex += 1
     }
     throw IndexOutOfBoundsException()
   }
 
   override fun remove(data: Int) {
-    if (headNode == null) throw NullPointerException()
+    if (head == null) throw NullPointerException()
 
-    var currentNode = headNode
+    var currentNode = head
 
     if (currentNode?.data == data) {
       removeFirst()
       return
     }
 
-    while (currentNode?.linkedNode != null) {
-      if (currentNode.linkedNode?.data == data) {
-        val link = currentNode.linkedNode?.linkedNode
-        currentNode.linkedNode?.linkedNode = null
-        currentNode.linkedNode = link
+    while (currentNode?.next != null) {
+      if (currentNode.next?.data == data) {
+        val link = currentNode.next?.next
+        currentNode.next?.next = null
+        currentNode.next = link
         return
       }
-      currentNode = currentNode.linkedNode
+      currentNode = currentNode.next
     }
     throw NullPointerException()
   }
 
-
-  // - - - - - - - - - - - - - - - Get - - - - - - - - - - - - - - -
-
   override fun getFirst(): Int {
-    val tempNode = headNode;
+    val tempNode = head;
     if (tempNode == null) throw NullPointerException() else return tempNode.data
   }
 
   override fun getLast(): Int {
-    if (headNode == null) throw NullPointerException()
+    if (head == null) throw NullPointerException()
 
-    var currentNode = headNode
-    while (currentNode?.linkedNode != null) currentNode = currentNode.linkedNode
+    var currentNode = head
+    while (currentNode?.next != null) currentNode = currentNode.next
     if (currentNode?.data != null) return currentNode.data else throw NullPointerException()
   }
 
   override fun getAtIndex(index: Int): Int {
-    if (headNode == null) throw NullPointerException()
+    if (head == null) throw NullPointerException()
     if (index < 0) throw IndexOutOfBoundsException()
 
-    var currentNode = headNode
+    var currentNode = head
     var currentIndex = 0
 
     while (currentNode != null && currentIndex <= index) {
       if (currentIndex == index) return currentNode.data
-      currentNode = currentNode.linkedNode
+      currentNode = currentNode.next
       currentIndex += 1
     }
     throw IndexOutOfBoundsException()
   }
 
-
-  // - - - - - - - - - - - - - - - Check - - - - - - - - - - - - - - -
-
   override fun contains(data: Int): Boolean {
-    if (headNode == null) throw NullPointerException()
+    if (head == null) throw NullPointerException()
 
-    var currentNode = headNode
+    var currentNode = head
 
     while (currentNode != null) {
       if (currentNode.data == data) return true
-      currentNode = currentNode.linkedNode
+      currentNode = currentNode.next
     }
     return false
   }
 
-
-  // - - - - - - - - - - - - - - - Object callbacks - - - - - - - - - - - - - - -
-
   override fun equals(other: Any?): Boolean {
-    if (other is MyLinkedList) {
-      val thisIterator = this.iterator()
-      val otherIterator = other.iterator()
+    if (other !is MyLinkedList) return false
 
-      while (thisIterator.hasNext() && otherIterator.hasNext()) {
-        if (thisIterator.next() == otherIterator.next()) continue else return false
-      }
+    var current = head
+    var otherCurrent = other.head
 
-      return (!thisIterator.hasNext() == !otherIterator.hasNext())
+    while (current != null && otherCurrent != null) {
+      if (current != otherCurrent) return false
+      current = current.next
+      otherCurrent = otherCurrent.next
     }
-    return false
+    return current == otherCurrent
   }
 
   override fun toString(): String {
-    if (headNode != null) {
-      return headNode.toString()
+    if (head != null) {
+      return head.toString()
     }
     return "empty"
   }
 
+  override fun hashCode(): Int {
+    var result = 1
+    var current = head
 
-  // - - - - - - - - - - - - - - - Iterable callbacks - - - - - - - - - - - - - - -
+    while (current != null) {
+      result = result * 31 + current.hashCode()
+      current = current.next
+    }
+    return result
+  }
 
   override fun iterator(): Iterator<Int> {
     return object : Iterator<Int> {
 
-      var currentNode = headNode;
+      var currentNode = head;
 
       override fun hasNext(): Boolean {
         return currentNode != null
@@ -315,22 +308,40 @@ class MyLinkedList() : GLinkedList<Int>, Iterable<Int> {
           throw NullPointerException()
 
         } else {
-          currentNode = currentNode?.linkedNode
+          currentNode = currentNode?.next
           return returnInt
         }
       }
     }
   }
 
-  // - - - - - - - - - - - - - - - Node data class - - - - - - - - - - - - - - -
+  fun isEmpty(): Boolean {
+    return head == null
+  }
 
-  private data class Node(var data: Int) {
-    var linkedNode: Node? = null
-
-    override fun toString(): String {
-      return "[" + data + "]" + " -> " + if (linkedNode == null) "null" else linkedNode.toString()
-    }
+  fun isNotEmpty(): Boolean {
+    return head != null
   }
 
 
+  // ---------------- Node data class ----------------
+
+  private data class Node(var data: Int) {
+    var next: Node? = null
+
+    override fun toString(): String {
+      return "[" + data + "]" + " -> " + if (next == null) "null" else next.toString()
+    }
+
+    override fun hashCode(): Int {
+      val nextData = next?.data ?: 0
+      return 31 * (data.hashCode() + nextData.hashCode())
+    }
+
+    override fun equals(other: Any?): Boolean {
+      if (other !is Node) return false
+
+      return other.data == data && other.next == next
+    }
+  }
 }
