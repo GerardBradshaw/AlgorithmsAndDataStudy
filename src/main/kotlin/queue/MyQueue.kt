1e76201197c450917ccabf062458f4795c5a7f20
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2020-03-13 17:37:13 -0700

    Rewrote equals() and hashCode() for MyQueue
    Refactored comments and variable names. Also broke insert() into smaller chunks to negate the need for explanatory comments.

--- src/main/kotlin/queue/MyQueue.kt
+++ src/main/kotlin/queue/MyQueue.kt
@@ -4,46 +4,36 @@ import java.lang.StringBuilder
 
 class MyQueue<T> {
 
+  // ---------------- Fields ----------------
+
   private var front: Node<T>? = null
   private var back: Node<T>? = null;
 
-  fun enqueue(data: T) {
-    val newNode = Node(data)
+
+  // ---------------- Public fun ----------------
+
+  fun enqueue(element: T) {
+    val newNode = Node(element)
     val frontHolder = front;
     val backHolder = back;
 
-    // Case: queue is empty -> set front as newNode
-    if (frontHolder == null)
-      front = newNode
-
-    else {
-      // Case: queue only contains front -> set back as newNode and link front to back
-      if (backHolder == null) {
-        back = newNode
-        frontHolder.prev = back
-        front = frontHolder
-
-        // Case: queue has a front and back already -> add newNode to back and link to it from the old back
-      } else {
-        backHolder.prev = newNode;
-        back = newNode;
-      }
+    when {
+      frontHolder == null -> insertFirst(newNode)
+      backHolder == null -> insertSecond(newNode, frontHolder)
+      else -> insertThirdAndBeyond(newNode, backHolder)
     }
   }
 
   fun dequeue(): T? {
-    // Case: queue is empty -> return null
     val frontHolder = front ?: return null
 
-    // Case: queue is not empty -> return front.data and set front as front.prev
     front = frontHolder.prev
     frontHolder.prev = null
     return frontHolder.data
-
   }
 
   override fun toString(): String {
-    val frontHolder = front ?: return "empty"
+    val frontHolder = front ?: return "[]"
     val builder = StringBuilder()
     var current: Node<T>? = frontHolder
 
@@ -57,26 +47,50 @@ class MyQueue<T> {
   }
 
   override fun equals(other: Any?): Boolean {
-    // Case: other is not a MyQueue
     if (other !is MyQueue<*>) return false
 
-    var current: Node<T>? = front
-    var otherData: Any?
+    var current = front
+    var otherCurrent = other.front
 
-    while (current != null) {
-      otherData = other.dequeue()
-
-      if (otherData == null || !otherData.equals(current.data))
-        return false
-
+    while (current != null && otherCurrent != null) {
+      if (current != otherCurrent) return false
       current = current.prev
+      otherCurrent = otherCurrent.prev
     }
-    return other.dequeue() == null
+    return current == otherCurrent
   }
 
   override fun hashCode(): Int {
-    return toString().hashCode()
+    var result = 1
+    var current = front
+
+    while (current != null) {
+      result = result * 31 + current.hashCode()
+      current = current.prev
+    }
+    return result
   }
+
+
+  // ---------------- Helpers ----------------
+
+  private fun insertFirst(node: Node<T>) {
+    front = node
+  }
+
+  private fun insertSecond(node: Node<T>, frontHolder: Node<T>) {
+    back = node
+    frontHolder.prev = back
+    front = frontHolder
+  }
+
+  private fun insertThirdAndBeyond(node: Node<T>, backHolder: Node<T>) {
+    backHolder.prev = node
+    back = node
+  }
+
+
+  // ---------------- Node data class ----------------
 
   private data class Node<T>(var data: T) {
 
@@ -85,6 +99,15 @@ class MyQueue<T> {
     override fun toString(): String {
       return data.toString();
     }
-  }
 
+    override fun equals(other: Any?): Boolean {
+      if (other !is Node<*> || other.prev?.data != data) return false
+
+      return other.data == data
+    }
+
+    override fun hashCode(): Int {
+      return 31 * (data.hashCode() + prev?.data.hashCode())
+    }
+  }
 }
