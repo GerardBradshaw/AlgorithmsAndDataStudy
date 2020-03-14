e28fa98c23b1a0f21267df979f9bc27ab840cb5d
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2020-03-13 17:39:06 -0700

    Rewrote equals() and hashCode() for MyStack
    Also:
    - refactored comments and variable names
    - Defined equals() and hashCode() for Node inner class

--- src/main/kotlin/stack/MyStack.kt
+++ src/main/kotlin/stack/MyStack.kt
@@ -4,17 +4,26 @@ import java.lang.NullPointerException
 
 class MyStack<T> : Collection<T> {
 
+  // ---------------- Fields ----------------
+
   private var top: Node<T>? = null
   private var base: Node<T>? = null
   private var height = 0
 
-  /**
-   * Pushes input data to the top of the stack.
-   */
-  fun push(data: T?) {
-    if (data == null) return
+  override val size: Int
+    get() = height
 
-    val newNode = Node(data)
+
+  // ---------------- Public fun ----------------
+
+  /**
+   * Pushes input element to the top of the stack.
+   * @param element the element to insert.
+   */
+  fun push(element: T?) {
+    if (element == null) return
+
+    val newNode = Node(element)
     val topHolder = top
     val baseHolder = base
 
@@ -25,29 +34,13 @@ class MyStack<T> : Collection<T> {
     }
   }
 
-  private fun addToEmptyStack(newNode: Node<T>) {
-    top = newNode
-    height++
-  }
-
-  private fun addToStackOfOne(top: Node<T>, newNode: Node<T>) {
-    base = top
-    newNode.prev = base
-    this.top = newNode
-    height++
-  }
-
-  private fun addToStackOfTwoOrMore(newNode: Node<T>) {
-    newNode.prev = top
-    top = newNode
-    height++
-  }
-
   /**
-   * Removes and returns the data from the top of the stack.
+   * Removes and returns the element from the top of the stack.
+   *
+   * @return the element at the top of the stack, or null if the stack is empty.
    */
-  fun pop(): T {
-    val topHolder = top ?: throw NullPointerException()
+  fun pop(): T? {
+    val topHolder = top ?: return null
     top = topHolder.prev
     topHolder.prev = null
     height--
@@ -55,52 +48,14 @@ class MyStack<T> : Collection<T> {
   }
 
   /**
-   * Returns the data from the top of the stack without removing it.
+   * Returns the element from the top of the stack without removing it.
+   *
+   * @return the element at the top of the stack, or null if the stack is empty.
    */
   fun peek(): T? {
     val topHolder = top ?: return null
     return topHolder.element
   }
-
-  override fun equals(other: Any?): Boolean {
-    if (other !is MyStack<*>) return false
-
-    var current = top
-    var otherData: Any?
-
-    while (current != null) {
-      otherData = other.pop()
-
-      if (otherData == null || !otherData.equals(current.element))
-        return false
-
-      current = current.prev
-    }
-    return other.pop() == null
-  }
-
-  override fun hashCode(): Int {
-    return toString().hashCode()
-  }
-
-  override fun toString(): String {
-    if (top == null) return "empty"
-
-    val builder = StringBuilder()
-    builder.append("TOP ")
-
-    var current = top
-
-    while (current != null) {
-      builder.append("<- [").append(current.toString()).append("] ")
-      current = current.prev
-    }
-
-    return builder.append("<- BASE").toString()
-  }
-
-  override val size: Int
-    get() = height
 
   override fun contains(element: T): Boolean {
     var current = top
@@ -123,6 +78,10 @@ class MyStack<T> : Collection<T> {
     return height == 0
   }
 
+  fun isNotEmpty(): Boolean {
+    return height != 0
+  }
+
   override fun iterator(): Iterator<T> {
     return object : Iterator<T> {
       var current = top
@@ -141,12 +100,87 @@ class MyStack<T> : Collection<T> {
     }
   }
 
+  override fun equals(other: Any?): Boolean {
+    if (other !is MyStack<*> || other.height != height) return false
+
+    var otherCurrent = other.top
+    var current = top
+
+    while (current != null && otherCurrent != null) {
+      if (otherCurrent != other) return false
+      current = current.prev
+      otherCurrent = otherCurrent.prev
+    }
+
+    return current == otherCurrent
+  }
+
+  override fun hashCode(): Int {
+    var result = 1
+    var current = top
+
+    while (current != null) {
+      result = result * 31 + current.hashCode()
+      current = current.prev
+    }
+    return result
+  }
+
+  override fun toString(): String {
+    if (top == null) return "[]"
+
+    val builder = StringBuilder()
+    builder.append("TOP ")
+
+    var current = top
+
+    while (current != null) {
+      builder.append("<- [").append(current.toString()).append("] ")
+      current = current.prev
+    }
+
+    return builder.append("<- BASE").toString()
+  }
+
+
+  // ---------------- Helpers ----------------
+
+  private fun addToEmptyStack(newNode: Node<T>) {
+    top = newNode
+    height++
+  }
+
+  private fun addToStackOfOne(top: Node<T>, newNode: Node<T>) {
+    base = top
+    newNode.prev = base
+    this.top = newNode
+    height++
+  }
+
+  private fun addToStackOfTwoOrMore(newNode: Node<T>) {
+    newNode.prev = top
+    top = newNode
+    height++
+  }
+
+
+  // ---------------- Node class ----------------
+
   private data class Node<T>(var element: T) {
 
     var prev: Node<T>? = null
 
     override fun toString(): String {
       return element.toString()
+    }
+
+    override fun equals(other: Any?): Boolean {
+      if (other !is Node<*>) return false
+      return other.element == element && other.prev?.element == prev?.element
+    }
+
+    override fun hashCode(): Int {
+      return 31 * (element.hashCode() + prev?.element.hashCode())
     }
   }
 }
