d902631cc2fee4fcd71a20c15b5b785d55037460
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2020-03-27 14:10:43 -0700

    Refactoring & bug fixes in MyMinHeap
    - Refactored field and function names
    - Updated layout to align with Kotlin standards
    - Broke methods down into smaller parts to increase readability
    - Fixed bug causing unnecessary resizing of array

--- src/main/kotlin/heap/MyMinHeap.kt
+++ src/main/kotlin/heap/MyMinHeap.kt
@@ -6,7 +6,7 @@ class MyMinHeap<T : Comparable<T>>() {
   // ---------------- Fields & Constructor ----------------
 
   private var array: Array<Any?> = arrayOfNulls<Any?>(3)
-  private var size = 0
+  private var numberOfElements = 0
 
   constructor(data: Array<T?>) : this() {
     for (obj in data) {
@@ -18,90 +18,60 @@ class MyMinHeap<T : Comparable<T>>() {
   // ---------------- Public fun ----------------
 
   fun insert(element: T?) {
-    if (element == null)
-      return
-
-    if (!isEnoughSpace())
-      resizeArray()
-
-    addToData(element)
+    if (element == null) return
+    resizeArrayIfRequired()
+    addToArray(element)
   }
 
   fun getMin(): T {
-    val max = array[0] ?: throw NullPointerException()
-    return max as T
+    return array[0] as T? ?: throw NullPointerException()
   }
 
   fun popMin(): T {
-    val max = array[0] ?: throw NullPointerException()
+    val result = array[0] as T? ?: throw NullPointerException()
     deleteMin()
-    return max as T
-  }
-
-  fun deleteMin() {
-    if (array[0] == null)
-      throw NullPointerException()
-
-    if (size == 0) {
-      return
-    }
-    else if (size == 1) {
-      array[0] = null
-      size--
-    }
-    else {
-      array[0] = array[size - 1]
-      array[size - 1] = null
-      size--
-
-      moveDownToCorrectPosition(0)
-    }
-
-    if (array.size > 3 * size)
-      resizeArray()
-
+    return result
   }
 
   fun size(): Int {
-    return size
+    return numberOfElements
   }
 
   fun isEmpty(): Boolean {
-    if (size == 0) return true
-    return false
+    return numberOfElements == 0
   }
 
   fun isNotEmpty(): Boolean {
-    return size != 0
+    return numberOfElements != 0
   }
 
   fun contains(element: T): Boolean {
-    for (i in 0 until size) {
+    for (i in 0 until numberOfElements) {
       if (array[i] == element) return true
     }
     return false
   }
 
   fun getAllUnordered(): Array<T> {
-    return Array<Any>(size) { index -> array[index] as T } as Array<T>
+    return Array<Any>(numberOfElements) { index -> array[index] as T } as Array<T>
   }
 
   override fun equals(other: Any?): Boolean {
     // TODO need to learn how to sort array
-    if (other !is MyMinHeap<*> || other.size != size) return false
+    if (other !is MyMinHeap<*> || other.numberOfElements != numberOfElements) return false
 
-    for (i in 0 until size) {
+    for (i in 0 until numberOfElements) {
       if (other.array[i] != array[i]) return false
     }
     return true
   }
 
   override fun toString(): String {
-    if (size == 0) return "[]"
+    if (numberOfElements == 0) return "[]"
 
     val builder = StringBuilder().append("[").append(array[0])
 
-    for (i in 1 until size) {
+    for (i in 1 until numberOfElements) {
       builder.append(", ").append(array[i].toString())
     }
 
@@ -112,7 +82,7 @@ class MyMinHeap<T : Comparable<T>>() {
     val prime = 31
     var result = 0
 
-    for (i in 0 until size) {
+    for (i in 0 until numberOfElements) {
       result = result * prime + array[i].hashCode()
     }
     return result
@@ -121,75 +91,111 @@ class MyMinHeap<T : Comparable<T>>() {
 
   // -------- Helpers --------
 
-  private fun isEnoughSpace(): Boolean {
-    return array.size >= size + 1
+  private fun deleteMin() {
+    when {
+      numberOfElements == 1 -> deleteMinSmallHeap()
+      numberOfElements > 1 -> deleteMinLargeHeap()
+    }
   }
 
-  private fun addToData(element: T) {
-    this.array[size] = element
-    size++;
-    moveUpToCorrectPosition(size - 1)
+  private fun deleteMinSmallHeap() {
+    array[0] = null
+    numberOfElements--
+  }
+
+  private fun deleteMinLargeHeap() {
+    val lastIndexNotNull = numberOfElements - 1
+
+    array[0] = array[lastIndexNotNull]
+    array[lastIndexNotNull] = null
+    numberOfElements--
+
+    moveDownToCorrectPosition(0)
+    resizeArrayIfRequired()
+  }
+
+  private fun addToArray(element: T) {
+    val insertionIndex = numberOfElements
+
+    array[insertionIndex] = element
+    numberOfElements++;
+
+    moveUpToCorrectPosition(insertionIndex)
+  }
+
+  private fun resizeArrayIfRequired() {
+    val minimumRequiredSize = numberOfElements + 2
+    val isRequired = array.size <= minimumRequiredSize || array.size > 2 * minimumRequiredSize
+    if (isRequired) resizeArray()
   }
 
   private fun resizeArray() {
-    val tempData: Array<Any?> = arrayOfNulls(size * 2)
-    System.arraycopy(array, 0, tempData, 0, size)
-    array = tempData
+    val largerArray: Array<Any?> = if (numberOfElements != 0) arrayOfNulls(numberOfElements * 2) else arrayOfNulls(4)
+    System.arraycopy(array, 0, largerArray, 0, numberOfElements)
+    array = largerArray
   }
 
-  private fun moveDownToCorrectPosition(index: Int) {
+  private fun reorderArrayWithTwoElements() {
+    val firstElement = array[0] as T
+    val secondElement = array[1] as T
+
+    if (firstElement > secondElement) {
+      swapElementsInArray(0, 1)
+    }
+  }
+
+  private fun reorderArrayWithMoreThanTwoElementsAfterDeletion(index: Int) {
     var currentIndex = index
     var leftIndex = leftIndexOf(currentIndex)
     var rightIndex = rightIndexOf(currentIndex)
-    var currentValue: T
-    var leftValue: T
-    var rightValue: T?
-    val maxIndex = size - 1
-
-    if (size == 2) {
-      currentValue = array[currentIndex] as T
-      leftValue = array[leftIndex] as T
-      if (currentValue > leftValue) swap(currentIndex, leftIndex)
-    }
+    val maxIndex = numberOfElements - 1
 
     while (leftIndex < maxIndex && rightIndex <= maxIndex) {
-      currentValue = array[currentIndex] as T? ?: break
-      leftValue = array[leftIndex] as T? ?: break
-      rightValue = array[rightIndex] as T?
+      val currentValue = array[currentIndex] as T? ?: break
+      val leftValue = array[leftIndex] as T? ?: break
+      val rightValue = array[rightIndex] as T?
 
-      if (rightValue == null || leftValue <= rightValue) {
+      if (rightValue == null) {
         if (currentValue > leftValue) {
-          swap(currentIndex, leftIndex)
+          swapElementsInArray(currentIndex, leftIndex)
           currentIndex = leftIndex
         }
         else return
       }
-      else if (rightValue < leftValue) {
+      else {
         if (currentValue > rightValue) {
-          swap(currentIndex, rightIndex)
+          swapElementsInArray(currentIndex, rightIndex)
           currentIndex = rightIndex
+        }
+        else if (currentValue > leftValue) {
+          swapElementsInArray(currentIndex, leftIndex)
+          currentIndex = leftIndex
         }
         else return
       }
-      else return
-
       leftIndex = leftIndexOf(currentIndex)
       rightIndex = rightIndexOf(currentIndex)
+    }
+  }
+
+  private fun moveDownToCorrectPosition(index: Int) {
+    when {
+      numberOfElements <= 1 -> return
+      numberOfElements == 2 -> reorderArrayWithTwoElements()
+      else -> reorderArrayWithMoreThanTwoElementsAfterDeletion(index)
     }
   }
 
   private fun moveUpToCorrectPosition(index: Int) {
     val value = array[index] as T? ?: return
     var newIndex = index
-    var parentIndex: Int
-    var parentValue: T?
 
     while (newIndex > 0) {
-      parentIndex = parentIndexOf(newIndex)
-      parentValue = array[parentIndex] as T?
+      val parentIndex = parentIndexOf(newIndex)
+      val parentValue = array[parentIndex] as T? ?: return
 
-      if (parentValue != null && parentValue > value) {
-        swap(newIndex, parentIndex)
+      if (parentValue > value) {
+        swapElementsInArray(newIndex, parentIndex)
         newIndex = parentIndex
       }
       else return
@@ -208,8 +214,8 @@ class MyMinHeap<T : Comparable<T>>() {
     return if (index % 2 != 0) ((index - 1) / 2) else ((index - 2) / 2)
   }
 
-  private fun swap(index1: Int, index2: Int) {
-    if (index1 >= size || index2 >= size) throw IndexOutOfBoundsException();
+  private fun swapElementsInArray(index1: Int, index2: Int) {
+    if (index1 >= numberOfElements || index2 >= numberOfElements) throw IndexOutOfBoundsException();
 
     val value1 = array[index1]
     array[index1] = array[index2]
