7650c1990928614c8910d434a73a712aae1ef328
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2020-04-06 16:04:13 -0700

    Completed question 1-2

--- src/main/kotlin/array/InterviewQuestions.kt
+++ src/main/kotlin/array/InterviewQuestions.kt
@@ -1,9 +1,12 @@
 package array
 
+import map.MyHashMap
+
 class InterviewQuestions {
 
   /**
    * Returns true if [string] has all unique characters. O(n) time, O(1) space. Assumes string is ASCII.
+   *
    * Other methods:
    * - Use a set to achieve O(n log(n)) time (needs extra space though)
    * - Sort the array using heap sort for O(n log(n)) time (no extra space needed!) if modification of string allowed.
@@ -17,6 +20,44 @@ class InterviewQuestions {
       val index = string[i].toInt()
       if (asciiCharInString[index]) return false
       asciiCharInString[index] = true
+    }
+    return true
+  }
+
+  /**
+   * Returns true if [string1] and [string2] are permutations of each other. O(n) time, O(n) space.
+   *
+   * Other methods:
+   * - Sort strings then check if they are equal for O(n log(n)) time.
+   * - Use an approach similar to [q1_1_isUnique] if character space is known.
+   */
+  fun q1_2_checkPermutation(string1: String, string2: String): Boolean {
+    if (string1.length != string2.length) return false
+
+    val charToCount = MyHashMap<Char, Int>()
+
+    for (c in string1) {
+      val prevCount = charToCount.get(c)
+      var newCount = 1
+
+      if (prevCount != null) {
+        newCount += prevCount
+        charToCount.remove(c)
+      }
+      charToCount.put(c, newCount)
+    }
+
+    for (c in string2) {
+      val prevCount = charToCount.get(c)
+
+      if (prevCount != null) {
+        if (prevCount == 1) charToCount.remove(c)
+        else {
+          charToCount.remove(c)
+          charToCount.put(c, prevCount - 1)
+        }
+      }
+      else return false
     }
     return true
   }
