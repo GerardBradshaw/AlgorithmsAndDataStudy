76a18ec03737d0b30de970aec155dd011c117090
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2020-06-19 12:08:40 -0700

    Created new QuickSort class

---
+++ src/main/kotlin/sort/MyQuickSorter2.kt
@@ -0,0 +1,40 @@
+package sort
+
+class MyQuickSorter2 {
+  fun sort(array: IntArray) {
+    quickSort(array, 0, array.size - 1)
+  }
+
+  private fun quickSort(array: IntArray, leftIndex: Int, rightIndex: Int) {
+    val index = partition(array, leftIndex, rightIndex)
+
+    if (leftIndex < index - 1) quickSort(array, leftIndex, index - 1)
+    if (rightIndex > index) quickSort(array, index, rightIndex)
+  }
+
+  private fun partition(array: IntArray, leftIndex: Int, rightIndex: Int): Int {
+    val mid = leftIndex + (rightIndex - leftIndex) / 2
+    val pivot = array[mid]
+
+    var left = leftIndex
+    var right = rightIndex
+
+    while (left < right) {
+      while (left <= rightIndex && array[left] < pivot) left++
+      while (right >= leftIndex && array[right] > pivot) right--
+
+      if (left <= right) {
+        swap(array, left, right)
+        left++
+        right--
+      }
+    }
+    return left
+  }
+
+  private fun swap(array: IntArray, i: Int, j: Int) {
+    val value1 = array[i]
+    array[i] = array[j]
+    array[j] = value1
+  }
+}
